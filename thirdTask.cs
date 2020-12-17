0f5c8f6804939fe98c07de6b5ac1aae3868407c6
daliokas <kundavicius@gmail.com>
2020-12-17 16:19:36 +0200

    Add files via upload
    3. Write a C# program that takes a word as an input and displays it reversed.

---
+++ thirdTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace thirdtask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your word");
+            var input = Console.ReadLine();
+            var charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            var reversed = new string(charArray);
+            Console.WriteLine(reversed);
+        }
+    }
+}
